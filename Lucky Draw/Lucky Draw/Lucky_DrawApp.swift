10bc2b9125d632c4f07a619992976ed2587442d2
Zhi Yong Huang <huangdanny21@gmail.com>
2023-02-25 20:41:18 -0800

    First init

---
+++ Lucky Draw/Lucky Draw/Lucky_DrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lucky_DrawApp.swift
+//  Lucky Draw
+//
+//  Created by Zhi Yong Huang on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Lucky_DrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lucky Draw/Lucky DrawUITests/Lucky_DrawUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lucky_DrawUITestsLaunchTests.swift
+//  Lucky DrawUITests
+//
+//  Created by Zhi Yong Huang on 2/25/23.
+//
+
+import XCTest
+
+final class Lucky_DrawUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
